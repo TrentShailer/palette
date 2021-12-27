cbb1bb30934af70ad192d2cf10280f73acbf081b
Trent Shailer <trent.shailer@gmail.com>
2021-12-27 19:25:05 +1300

    Standard http server setup

--- palette-backend/index.js
+++ palette-backend/index.js
@@ -1,1 +1,24 @@
-// 2002
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+
+const express = require("express");
+let app = express();
+
+let httpServer = http.createServer(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/build")));
+
+app.set("trust proxy", 1);
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html");
+});
+
+app.get("*", (req, res) => {
+	console.log(req.path);
+	res.redirect("/");
+});
+
+httpServer.listen(2002);
