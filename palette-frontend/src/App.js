2861f6fa833cc87db615a020fe116d472b2b521a
Trent Shailer <trent.shailer@gmail.com>
2021-12-27 19:45:33 +1300

    loaded palette file

--- palette-frontend/src/App.js
+++ palette-frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import Palette from "./Palette";
+
 function App() {
 	return <div className="App"></div>;
+}
+
+function ColourSet({ normalArray, altArray, name }) {
+	return <div></div>;
 }
 
 export default App;
